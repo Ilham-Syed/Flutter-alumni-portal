ca9d35b0ec25b5fb9f3b501076a47802ac0ef50a
Ilham-Syed <ilhamsyed1310@gmail.com>
2023-02-05 13:08:14 +0530

    Added firestore database connection

--- lib/Profile_page_screens/preferred_domain.dart
+++ lib/Profile_page_screens/preferred_domain.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_prjcts/screens/maindrawer.dart';
@@ -10,26 +11,38 @@ class preferred_domain extends StatefulWidget {
 }
 
 class _preferred_domainState extends State<preferred_domain> {
-  final _nameController=TextEditingController();
-  final _domainController=TextEditingController();
+  final _nameController = TextEditingController();
+  final _domainController = TextEditingController();
+  CollectionReference users =
+      FirebaseFirestore.instance.collection('userDomainPref');
+  String name="";
+  String domain="";
   /*final List<String> _domains = ['Web development', 'App development', 'Machine learning','Cryptography','Hardware','Others'];
   String _selectedDomain='';*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          elevation: 0,
-          title: const Text("Preferred Domains",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+        title: const Text(
+          "Preferred Domains",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
         ),
+      ),
       drawer: MainDrawer(),
       body: Container(
         padding: const EdgeInsets.all(20),
         child: Column(
           children: <Widget>[
-            TextField(controller: _nameController,
-            decoration: InputDecoration(labelText: "Enter your Name",border: OutlineInputBorder()),
-
+            TextField(
+              onChanged: (value){
+                name=value;
+              },
+              controller: _nameController,
+              decoration: InputDecoration(
+                  labelText: "Enter your Name", border: OutlineInputBorder()),
             ),
             SizedBox(
               height: 30,
@@ -52,17 +65,34 @@ class _preferred_domainState extends State<preferred_domain> {
               ),
             ),*/
             TextField(
+              onChanged: (value){
+                domain=value;
+              },
               controller: _domainController,
               decoration: InputDecoration(
-                labelText: "Preferred Domain",
-                border: OutlineInputBorder()
-              ),
+                  labelText: "Preferred Domain", border: OutlineInputBorder()),
             ),
             SizedBox(height: 40),
-            ElevatedButton(onPressed:(){
-              Navigator.pop(context);
-            }
-                , child: Text("Submit"))
+            ElevatedButton(
+                onPressed: () async {
+                  await users.add({'Name': name, 'Domain': domain}).then(
+                      (value) => print('User Added'));
+                  showDialog(context: context,
+                      builder: (BuildContext context){
+                    return AlertDialog(
+                      title: Text('Updated!!'),
+                      content: Text("Your data has been succesfully added to the database"),
+                      actions: [
+                        TextButton(onPressed: (){
+                          Navigator.of(context).pop();
+                        }, child: Text('OK'))
+                      ],
+                    );
+                      });
+                  _nameController.clear();
+                  _domainController.clear();
+                },
+                child: Text("Submit"))
           ],
         ),
       ),
